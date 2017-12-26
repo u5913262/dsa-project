1a90a9fe2c6d9c47f9b66a4226c3c7ba4dd152b2
Prutchakorn <Prutchakron_1212_1212@hotmail.com>
2017-12-26 20:05:46 +0700

    kruskle input

--- kruskle.py
+++ kruskle.py
@@ -51,13 +51,10 @@ class kruskleMST:
 
 graph = kruskleMST(5)
 
-graph.addEdge(0, 1, 10)
-graph.addEdge(0, 2, 6)
-graph.addEdge(0, 3, 5)
-graph.addEdge(1, 3, 15)
-graph.addEdge(2, 3, 4)
-graph.addEdge(3, 4, 3)
-graph.addEdge(3, 1, 5)
-graph.addEdge(5, 3, 12)
-
+data = []
+for i in input().split(","):
+    data.append(i)
+    if len(data) == 3:
+        graph.addEdge(int(data[0]), int(data[1]), int(data[2]))
+        data = []
 graph.kruskle()
